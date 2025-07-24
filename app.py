98f51b4f8c343ab86c3cf14d799a747911b4f22e
harshVardhanLab <harshvardhan02102002@gmail.com>
2025-07-24 07:09:39 +0530

    vercel-json

--- app.py
+++ app.py
@@ -55,5 +55,7 @@ def get_messages():
     return jsonify(messages), 200
 
 if __name__ == "__main__":
-    init_db()  # Initialize DB before starting the app
-    app.run(debug=True, port=4000)
+    init_db()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
